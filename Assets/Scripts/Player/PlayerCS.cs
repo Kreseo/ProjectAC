dea1ce641f8677254109dbb8fa26442c8225b89d
Kreseo <supershadow_2@hotmail.com>
2020-12-24 00:20:38 -0500

    Add store animation

--- Assets/Scripts/Player/PlayerCS.cs
+++ Assets/Scripts/Player/PlayerCS.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class PlayerCS : MonoBehaviour
 {
+    enum PlayerState
+    {
+        Idle,
+        Store,
+        Inventory,
+        Status
+    }
+
+    enum PlayerDirection
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+    private PlayerState currentPlayerState;
+    private PlayerDirection currentPlayerDirection;
+    public bool nearNPC;
+
+    //References
+    public Animator storeAnimator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentPlayerState = PlayerState.Idle;
+        currentPlayerDirection = PlayerDirection.Right;
+        nearNPC = true;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region Movement
+        
+        if(currentPlayerState == PlayerState.Idle) 
+        {
+            if (nearNPC) 
+            {
+                if(Input.GetKey("e"))
+                {
+                    PlayerOpenCloseStore(true);
+                }
+            }
+        }
+        #endregion
+
+        #region Store  
+
+        if (currentPlayerState == PlayerState.Store)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                PlayerOpenCloseStore(false);
+            }
+        }
+        #endregion
+
+    }
+
+    public void PlayerOpenCloseStore(bool condition)
+    {
+        if(condition)
+        {
+            currentPlayerState = PlayerState.Store;
+            storeAnimator.SetBool("Open", true);
+        }
+        else 
+        {
+            currentPlayerState = PlayerState.Idle;
+            storeAnimator.SetBool("Open", false);
+        }
         
     }
 }
